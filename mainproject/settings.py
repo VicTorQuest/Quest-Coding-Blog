5ee172c465277cce799d394a8de4547850760456
Victor <victorokolie001@gmail.com>
2025-10-14 13:56:05 +0100

    more changes

--- mainproject/settings.py
+++ mainproject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =  True#str(os.environ.get('DEBUG')) == '1'
+DEBUG = str(os.environ.get('DEBUG')) == '1'
 
 ALLOWED_HOSTS = []
 
